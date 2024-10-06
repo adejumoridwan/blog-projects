205d2af8499cb6eff5580bdd6a214d2185e70665
Adejumo Ridwan Suleiman <adejumo999@gmail.com>
2024-10-06 11:45:35 +0000

    a

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from utilis import fetch_data, total_profit, total_revenue
+import pandas as pd
+
+# Business Logic
+data = fetch_data()
+
+def test_total_profit():
+    assert total_profit(data) == 15379.369
+
+def test_total_revenue():
+    assert total_revenue(data) == 307587.38
