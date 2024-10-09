f1d8da0589ab6e315a8ec9eeef15abd72877458b
Adejumo Ridwan Suleiman <adejumo999@gmail.com>
2024-10-09 11:26:55 +0100

    a

--- test_main.py
+++ test_main.py
@@ -6,8 +6,8 @@ data = fetch_data()
 
 
 def test_total_profit():
-    assert total_profit(data) == 1579.369
+    assert total_profit(data) == 15379.369
 
 
 def test_total_revenue():
-    assert total_revenue(data) == 307587
+    assert total_revenue(data) == 307587.38
